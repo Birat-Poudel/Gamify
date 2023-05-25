c9b8e0afd4a088d617bd5e7dca04324effe83460
Birat Poudel <poudel.birat25@gmail.com>
2023-05-25 16:07:32 +0545

    Building Loading Skeletion

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
     const index = url.indexOf('media/') + 'media/'.length
-    return url.slice(0, index) + 'crop/600/400' + url.slice(index)
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 
 }
 
